da7684ec0e19eccf3d0010c2976f1d1ca618b0b7
lulu <lukathrein94@gmail.com>
2023-09-19 00:35:31 +0200

    feat: deploy-commands

---
+++ src/deployment/deploy-commands.ts
@@ -0,0 +1,39 @@
+import {
+  REST,
+  RESTPostAPIChatInputApplicationCommandsJSONBody,
+  Routes,
+} from 'discord.js';
+import { CommandCollectionModule } from '../command/commandCollection';
+
+function loadCommands(): RESTPostAPIChatInputApplicationCommandsJSONBody[] {
+  var commands = new CommandCollectionModule().moduleList;
+  return commands.map((command) => command.data.toJSON());
+}
+
+const commands = loadCommands();
+
+// Construct and prepare an instance of the REST module
+const rest = new REST().setToken(process.env.BOT_TOKEN);
+
+// and deploy your commands!
+(async () => {
+  try {
+    console.log(
+      `Started refreshing ${commands.length} application (/) commands.`,
+    );
+
+    // The put method is used to fully refresh all commands in the guild with the current set
+    const data = await rest.put(
+      Routes.applicationGuildCommands(
+        process.env.CLIENT_ID,
+        process.env.SERVER_ID,
+      ),
+      { body: commands },
+    );
+
+    console.log(`Successfully reloaded ${data} application (/) commands.`);
+  } catch (error) {
+    // And of course, make sure you catch and log any errors!
+    console.error(error);
+  }
+})();
