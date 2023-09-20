98876f387c4efd4e2ee23800b0678aa0bd26498b
lulu <lukathrein94@gmail.com>
2023-09-21 01:46:01 +0200

    cleanup: move express to seperate module

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,8 @@ import {
 } from 'discord.js';
 import { config } from 'dotenv';
 import { CommandModule } from './modules/command/command.module';
-import { Utils } from './helpers/utils';
-import { verifyKeyMiddleware } from 'discord-interactions';
-import { Express, Request, Response } from 'express-serve-static-core';
 import { EventModule } from './modules/event/event.module';
-const express = require('express');
-const app: Express = express();
+import { ExpressModule } from './modules/interaction/express.module';
 
 function main(args: string[]) {
   config();
@@ -28,57 +24,9 @@ function main(args: string[]) {
 
   new CommandModule();
   new EventModule().init(client);
-  initExpress(app, port);
+  new ExpressModule().init(port);
 
   client.login(process.env.BOT_TOKEN);
-}
-
-function initExpress(app, port: number | string) {
-  app.use(
-    express.json({
-      verify: Utils.verifyDiscordRequest(process.env.PUBLIC_KEY),
-    }),
-  );
-
-  app.listen(port, () => {
-    console.log('Listening on port', port);
-  });
-
-  app.post(
-    '/interactions',
-    verifyKeyMiddleware(process.env.APP_PUBLIC_KEY),
-    (req: Request, res: Response) => {
-      const { type, data } = req.body;
-
-      /**
-       * Handle verification requests
-       */
-      if (type === InteractionType.Ping) {
-        return res.send({ type: InteractionResponseType.Pong });
-      }
-
-      if (type === InteractionType.ApplicationCommand) {
-        const { name } = data;
-        switch (name) {
-          case 'test':
-            // Send a message into the channel where command was triggered from
-            return res.send({
-              type: InteractionResponseType.ChannelMessageWithSource,
-              data: {
-                content: 'hello world ' + Utils.getRandomEmoji(),
-              },
-            });
-          default:
-            return res.send({
-              type: InteractionResponseType.ChannelMessageWithSource,
-              data: {
-                content: 'Hello world',
-              },
-            });
-        }
-      }
-    },
-  );
 }
 
 main(process.argv.slice(2));
