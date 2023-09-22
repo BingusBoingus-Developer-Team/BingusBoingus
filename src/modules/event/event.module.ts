12d7d219c289c08b2b20f95320efefbeabb6bfa8
lulu <lukathrein94@gmail.com>
2023-09-22 15:46:39 +0200

    fix(CommandModule): listen for commands

--- src/modules/event/event.module.ts
+++ src/modules/event/event.module.ts
@@ -1,15 +1,16 @@
 import { ACollectionModule } from '../../helpers/abstract/collection.module.abstract';
+import { CommandModule } from '../command/command.module';
 import { AEvent } from './event.abstract';
 import { ClientReady } from './services/clientReady';
 import { Interaction } from './services/interaction';
 import { MessageEvent } from './services/messageEvent';
 
 class EventModule extends ACollectionModule<any, AEvent> {
-  constructor() {
+  constructor(commandModule: CommandModule) {
     const moduleList: AEvent[] = [
       new ClientReady(),
       new MessageEvent(),
-      new Interaction(),
+      new Interaction(commandModule),
     ];
 
     super(moduleList);
