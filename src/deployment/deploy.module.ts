76fe7a9dcf3502e13215a28477fff2aed6bd01c0
Matteo Juen <matteo.juen@ematric.com>
2023-09-25 09:27:47 +0200

    feat: use config service for deployment

--- src/deployment/deploy.module.ts
+++ src/deployment/deploy.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DeployServcice } from './deploy.service';
 import { CommandModule } from '../modules/command/command.module';
+import { AppConfigModule } from '../config/config.module';
 
 @Module({
-  imports: [CommandModule],
+  imports: [CommandModule, AppConfigModule],
   providers: [DeployServcice],
 })
 export class DeployModule {}
--- src/deployment/deploy.service.ts
+++ src/deployment/deploy.service.ts
@@ -1,14 +1,15 @@
-import {
-  REST,
-  RESTPostAPIChatInputApplicationCommandsJSONBody,
-  Routes,
-} from 'discord.js';
+import { REST, RESTPostAPIChatInputApplicationCommandsJSONBody, Routes } from 'discord.js';
 import { Injectable } from '@nestjs/common';
 import { CommandService } from '../modules/command/command.service';
+import { AppConfigService } from '../config/config.service';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class DeployServcice {
-  constructor(private readonly commandService: CommandService) {
+  constructor(
+    private readonly commandService: CommandService,
+    private readonly configService: AppConfigService,
+  ) {
     var commands = this.loadCommands();
     this.deployCommands(commands);
   }
@@ -18,26 +19,18 @@ export class DeployServcice {
     return commands.map((command) => command.data.toJSON());
   }
 
-  async deployCommands(
-    commands: RESTPostAPIChatInputApplicationCommandsJSONBody[],
-  ) {
+  async deployCommands(commands: RESTPostAPIChatInputApplicationCommandsJSONBody[]) {
     // Construct and prepare an instance of the REST module
-    const rest = new REST().setToken(process.env.BOT_TOKEN);
+    const rest = new REST().setToken(this.configService.botToken);
 
     // and deploy your commands!
     try {
-      console.log(
-        `Started refreshing ${commands.length} application (/) commands.`,
-      );
+      console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
       // The put method is used to fully refresh all commands in the guild with the current set
-      const data = await rest.put(
-        Routes.applicationGuildCommands(
-          process.env.CLIENT_ID,
-          process.env.SERVER_ID,
-        ),
-        { body: commands },
-      );
+      const data = await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.SERVER_ID), {
+        body: commands,
+      });
 
       var log = '';
       if (Array.isArray(data)) log = data.length + ' ';
