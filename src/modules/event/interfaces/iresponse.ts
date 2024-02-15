cc1d43447df144f2cb72ed1b1b1bf46568ac9197
Matteo Juen <matteo.juen@ematric.com>
2024-02-15 10:28:48 +0100

    feat: new commands and response types

--- src/modules/event/interfaces/iresponse.ts
+++ src/modules/event/interfaces/iresponse.ts
@@ -1,4 +1,10 @@
 export interface IResponse {
   matcher: RegExp;
   response: string | Object;
+  responseType?: ResponseType;
+}
+
+export enum ResponseType {
+  Reply = 'reply',
+  Message = 'message',
 }
