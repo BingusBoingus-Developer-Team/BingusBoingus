16c85de4d92d8b29e9e34a67e48f6cac9de7f4b5
lulu12329 <lukathrein94@gmail.com>
2024-02-26 18:41:03 +0100

    test(interaction): provide PollService not DbPollService

--- src/modules/event/services/interaction.spec.ts
+++ src/modules/event/services/interaction.spec.ts
@@ -1,22 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { Interaction } from './interaction';
 import { CommandService } from '../../command/command.service';
-import { DbPollService } from '../../poll/service/db-poll.service';
+import { PollService } from '../../poll/service/poll.service';
 
 describe('Interaction', () => {
   var service: Interaction;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [Interaction,
-      {
-        provide: CommandService,
-        useValue: {}
-      },
-      {
-        provide: DbPollService,
-        useValue: {}
-      }],
+      providers: [
+        Interaction,
+        {
+          provide: CommandService,
+          useValue: {},
+        },
+        {
+          provide: PollService,
+          useValue: {},
+        },
+      ],
     }).compile();
 
     service = module.get<Interaction>(Interaction);
