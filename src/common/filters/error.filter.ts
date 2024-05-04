9bf5e49948a1a2cd8cc116b68f1f7cce6191c191
matteom0165 <matteo.juen@ematric.com>
2024-05-04 13:47:40 +0200

    feat(common): app api, res model, decorators and errorfilter

---
+++ src/common/filters/error.filter.ts
@@ -0,0 +1,45 @@
+import {
+    ExceptionFilter,
+    Catch,
+    HttpException,
+    ArgumentsHost,
+    HttpStatus,
+  } from '@nestjs/common';
+  import { ReE } from '../res.model';
+  
+  @Catch()
+  export class ErrorFilter implements ExceptionFilter {
+    catch(error: Error, host: ArgumentsHost) {
+      const ctx = host.switchToHttp();
+      const response = ctx.getResponse();
+      const request = ctx.getRequest<Request>();
+      const statusCode: number =
+        error instanceof HttpException
+          ? error.getStatus()
+          : HttpStatus.INTERNAL_SERVER_ERROR;
+  
+      let message;
+      console.error(error);
+      if (error instanceof HttpException) {
+        const errorMessage = (error.getResponse() as any)?.message;
+        message = Array.isArray(errorMessage) ? errorMessage : [errorMessage];
+      } else {
+        message = [error.message];
+      }
+  
+      const name =
+        error instanceof HttpException
+          ? (error.getResponse() as any)?.error || error.name
+          : error.name;
+  
+      const responseWithStatusCode = response.status(statusCode);
+      const errorPayload = ReE.FromData(statusCode, name, message);
+      // watch out for fastify
+      if (typeof responseWithStatusCode.json === 'undefined') {
+        responseWithStatusCode.send(errorPayload);
+      } else {
+        responseWithStatusCode.json(errorPayload);
+      }
+    }
+  }
+  
